21d9d95b8c0a6b20d946c955d4f0499d5a59ab7a
Mikeccx <545860057@qq.com>
2019-09-10 23:08:00 +0800

    添加双向绑定

--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -5,9 +5,13 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    "text": 'cx'
   },
-
+  change: function () {
+    this.setData({
+      text: 'ppp'
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -62,5 +66,14 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  inputedit: function (e) {
+    // 1. input 和 info 双向数据绑定
+    let dataset = e.currentTarget.dataset;
+    //data-开头的是自定义属性，可以通过dataset获取到，dataset是一个json对象，有obj和item属性，可以通过这两个实现双向数据绑定，通过更改这两个值，对不同name的变量赋值
+    let value = e.detail.value;
+    this.setData({
+      text: value
+    });
   }
 })
