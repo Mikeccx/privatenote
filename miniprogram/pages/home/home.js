8d223e7b459bedb9fe56a7ba1f217c814a4d6a15
Mikeccx <545860057@qq.com>
2019-09-12 17:13:04 +0800

    first-version

--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -1,17 +1,20 @@
 // pages/home/home.js
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    "text": 'cx'
+    "text": 'cx',
+    "note": []
   },
   change: function () {
     this.setData({
       text: 'ppp'
     })
   },
+  //跳转到写日志
   toaddnote: function () {
      wx.navigateTo({
        url: '../writenote/writenote',
@@ -20,11 +23,17 @@ Page({
        complete: function(res) {},
      })
   },
+  //跳转到页面详情
+  tonotespc: function (e) {
+    console.log(e.currentTarget.dataset.idx)
+    wx.navigateTo({
+      url: '../writenote/writenote?id='+ e.currentTarget.dataset.idx
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
   },
 
   /**
@@ -38,14 +47,26 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    console.log(app.globalData.openid);
+    const db = wx.cloud.database();
+    var that = this;
+    db.collection('note').get({
+      success: function (res) {
+        // res.data 是一个包含集合中有权限访问的所有记录的数据，不超过 20 条
+        console.log(res.data)
+        that.setData({
+          note:res.data
+        })
+      }
+    })
+    console.log(this.note)
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+    
   },
 
   /**
--- miniprogram/pages/writenote/writenote.js
+++ miniprogram/pages/writenote/writenote.js
@@ -5,70 +5,126 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    date: '',
+    id: '',
+    notespc: ''
   },
-  
-  send: function (e) {
+  // onLoad: function(options) {
+  //   if (app.globalData.openid) {
+  //     this.setData({
+  //       openid: app.globalData.openid
+  //     })
+  //   }
+  // },
+  //写日志
+  onAdd: function(e) {
+    const db = wx.cloud.database();
+    let now = new Date();
+    let time = now.toLocaleString();
+    if (e.detail.value !== '') {
+      return;
+    } else {
+      db.collection('note').add({
+        data: {
+          notespc: e.detail.value,
+          time: time
+        },
+        success: res => {
+          // 在返回结果中会包含新创建的记录的 _id
+          this.setData({
+            counterId: res._id
+          })
+          wx.showToast({
+            title: '已保存',
+          })
+          console.log('[数据库] [新增记录] 成功，记录 _id: ', res._id)
+        },
+        fail: err => {
+          wx.showToast({
+            icon: 'none',
+            title: '保存'
+          })
+          console.error('[数据库] [新增记录] 失败：', err)
+        }
+      })
+    }
+  },
+  send: function(e) {
     wx.showToast({
       title: '保存成功',
       icon: 'success',
       duration: 2000
     })
-     console.log(e.detail.value)
+    console.log(e.detail.value)
   },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function(options) {
+    const db = wx.cloud.database();
+    console.log('options')
+    console.log(Object.keys(options).length)
+    let that = this
+    if (Object.keys(options).length != 0) {
+      console.log('coming')
+      db.collection('note').doc(options.id).get({
+        success: function (res) {
+          // res.data 包含该记录的数据
+          that.setData({
+            notespc: res.data.notespc
+          })
+          console.log(res.data.notespc)
+        }
+      })
+    }
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-
+  onShow: function(options) {
+    console.log(options)
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
